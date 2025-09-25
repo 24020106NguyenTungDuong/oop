7d7579ad9e68feda2797f3115023c4d5bacf940b
Duo <24020106@vnu.edu.vn>
2025-09-25 20:45:07 +0700

    Introduction

---
+++ HW1/Introduction.java
@@ -0,0 +1,10 @@
+public class Introduction {
+    public static void main(String[] args) {
+        System.out.println("Nguyễn Tùng Dương\t24020106\tINT 2204 6\t24020106NguyenTungDuong\t24020106@vnu.edu.vn");
+        for (int i = 9; i > 0; i--) {
+            System.out.println(i + " bottles of beer on the wall, " + i + " bottles of beer.");
+            System.out.println("Take one down, pass it around, ");
+        }
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}
